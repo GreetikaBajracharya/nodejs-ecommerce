d5034dea34c4400f9849348ad5d20df80e66d873
Greetika <bajracharyagreetika1@gmail.com>
2025-01-16 21:54:20 +0545

    cart route and cart controller added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 
 import connectMongoDB from "./lib/db.js";
 
@@ -12,13 +13,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 2000;
 
-console.log(process.env.MONGO_URI);
-
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
