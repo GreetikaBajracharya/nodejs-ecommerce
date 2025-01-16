66f541e071fe625c25425f3e8079724780a72518
Greetika <bajracharyagreetika1@gmail.com>
2025-01-16 22:20:14 +0545

    coupon route and coupon controller added

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -10,3 +10,31 @@ export const getCoupon = async (req, res) => {
         res.status(500).json({message: "Internal server error"});
     }
 };
+
+export const validateCoupon = async (req, res) => {
+    try {
+        const {code} = req.body;
+
+        const coupon = await Coupon.findOne({code:code, userId: req.user._id, isActive: true});
+
+        if(!coupon) {
+            res.status(404).json({message: "Coupon not found"});
+        }
+
+        if(coupon.expireDate < new Date()) {
+            coupon.isActive = false;
+            await coupon.save();
+            res.status(404).json({message: "Coupon expired"});
+        }
+            
+        res.json({
+            message: "Coupon is valid",
+            code: coupon.code,
+            discountPercentage: coupon.discountPercentage
+        });
+
+    } catch (error) {
+        console.log("Error in validate coupon controller",error);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
