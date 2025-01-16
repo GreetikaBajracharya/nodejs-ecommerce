73891505b37b4f9b2a2b64991909752444b9bff8
Greetika <bajracharyagreetika1@gmail.com>
2025-01-16 11:28:41 +0545

    featured products

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -11,3 +11,28 @@ export const getAllProducts = async (req,res) => {
     }
 };
 
+export const getFeaturedProducts = async (req,res) => {
+    try {
+        let featuredProducts = await redis.get("featured_products");
+        if(featuredProducts) {
+           return res.json(JSON.parse(featuredProducts));
+        }
+        
+        //if not in redis, fetch from mongodb
+        //.lean() returns plain js obj instead of mongodb doc which is good for performance
+        featuredProducts = await Product.find({isFeatured: true}).lean();
+
+        if(!featuredProducts) {
+            return res.status(404).json({message: "Featured products not found"});
+        }
+
+        //store in redis for future quick access
+        await redis.set("featured_products", JSON.stringify(FeaturedProducts));
+        res.json(featuredProducts);
+    } catch (error) {
+        console.log("Error in get featured products controller",error);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+
+
