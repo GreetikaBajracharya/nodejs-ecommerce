11ceecd39f49a10f260549341887ff378b516e31
Greetika <bajracharyagreetika1@gmail.com>
2025-01-25 16:59:37 +0545

    getprofile added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -137,4 +137,11 @@ export const refreshToken = async (req,res) => {
     } 
 };
 
-
+export const getProfile = async (req,res) => {
+    try {
+          res.json(req.user);
+    } catch (error) {
+         console.log("Error in get profile controller",error);
+         res.status(500).json({message: "Internal server error"});
+    }
+};
